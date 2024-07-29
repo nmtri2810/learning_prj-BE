b9beb99e898cf1e417aede072df74891b50ef7c4
trinm-3423 <nguyen.minh.tri@sun-asterisk.com>
2024-07-29 13:48:51 +0700

    feat: add postgresql db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './modules/users/users.module';
+import typeormConfig from './config/typeorm.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+      load: [typeormConfig],
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: async (configService: ConfigService) =>
+        configService.get('typeorm'),
+      inject: [ConfigService],
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
